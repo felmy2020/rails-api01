c3ad1aba5eab4bcbd9d4526a67fa492ea212b885
felmy2020 <fyen.work@gmail.com>
2022-06-24 16:57:37 +0900

    api実装

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
+
+  ## ======= 👇 ここから変更する =======
+  resources :messages, only: ['index'] do
+    member do
+      resources :likes, only: ['create']
+    end
+  end
+  ## ====== 👆 ここまで変更する =======
+  resources :likes, only: ['destroy']
 end
